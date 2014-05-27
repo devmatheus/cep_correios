c86c0cb923c7864ec25999a45473a136183efe87
Gustavo Rafael <gustavorafael93@gmail.com>
2014-05-27 11:23:33 -0300

    Refactoring
    * Alternativa para envio de Post Request sem o extensão CUrl
    * Uso do DomXPath
    * Lógica para obter informações do HTML
    * Notificação de novas mensagens de erro

--- correios.php
+++ correios.php
@@ -2,40 +2,83 @@
 
 header('Content-Type: application/json; charset=utf-8');
 
-if (!$_GET['cep']) {
+if (!isset($_GET['cep'])) {
+  die(json_encode(array('erro' => 1, 'msg' => 'CEP não definido')));
+}
+
+if (empty($_GET['cep'])) {
   die(json_encode(array('erro' => 1, 'msg' => 'CEP vazio')));
 }
 
-$ch = curl_init('http://m.correios.com.br/movel/buscaCepConfirma.do');
-curl_setopt ($ch, CURLOPT_POST, 1);
-curl_setopt ($ch, CURLOPT_POSTFIELDS, http_build_query(array('cepEntrada' => $_GET['cep'], 'tipoCep'=>", 'cepTemp'=>", 'metodo'=>'buscarCep')));
-curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
-curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt ($ch, CURLOPT_ENCODING, 'UTF-8');
-$html = curl_exec ($ch);
+$cep = $_GET['cep'];
+
+
+$urlCorreios = 'http://m.correios.com.br/movel/buscaCepConfirma.do';
+
+$postFields = array(
+    'cepEntrada' => $cep, 
+    'tipoCep'=>'', 
+    'cepTemp'=>'', 
+    'metodo'=>'buscarCep'
+);
+
+
+if (extension_loaded('curl') && false) { //FIXME
+
+    $ch = curl_init($urlCorreios);
+    curl_setopt ($ch, CURLOPT_POST, 1);
+    curl_setopt ($ch, CURLOPT_POSTFIELDS, $postFields);
+    curl_setopt ($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt ($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt ($ch, CURLOPT_ENCODING, 'UTF-8');
+    $html = curl_exec ($ch);
+    
+} else {
+    $request = array('http' =>
+        array(
+            'method'  => 'POST',
+            'header'  => 'Content-type: application/x-www-form-urlencoded',
+            'content' => http_build_query($postFields)
+        )
+    );
+
+    $context = stream_context_create($request);
+
+    $html = file_get_contents($urlCorreios, false, $context);
+}
+
+if ($html === false) {
+    die(json_encode(array('erro' => 1, 'msg' => 'Erro ao obter dados')));
+}
 
 $doc = new DOMDocument();
-$doc->loadHTML($html);
-$doc->preserveWhiteSpace = false;
-$spans = $doc->getElementsByTagName('span');
 
-$dicionario = array('logadouro', 'bairro', 'cidade');
-$i=0;
-foreach ($spans as $span) {
-  if ($span->getAttribute('class') == 'respostadestaque') {
-    $resultados[$dicionario[$i]] = trim(preg_replace('/\s{2,}/',' ', $span->nodeValue));
-    $i++;
-    if ($i==3) break;
-  }
-}
-      
-if (!$resultados['logadouro'] && !$resultados['bairro']) {
-  die(json_encode(array('erro' => 1, 'msg' => 'CEP não encontrado ou Serviço dos correios offline')));
+@$doc->loadHTML($html);
+
+$xpath = new DomXPath($doc);
+
+$nodes = $xpath->query("//span[@class='respostadestaque']");
+
+$totRegistros = $nodes->length / 4;
+
+
+if (0 == $totRegistros) {
+    die(json_encode(array('erro' => 1, 'msg' => 'Sem registros (CEP não encontrado)')));
 }
 
-$str = explode('/', $resultados['cidade']); // Cidade /UF
-$resultados['cidade'] = trim($str[0]); // cidade
-$i++;
-$resultados['estado'] = trim($str[1]); // estado
+$arrResultado = array();
+$arrInfo = array('logadouro', 'bairro', 'cidade', 'cep');
 
-echo json_encode($resultados);
+for ($registro = 0; $registro < $totRegistros; $registro++) {
+
+    $arrResultado[$registro + 1] = array();
+    
+    foreach ($arrInfo as $index => $value) {
+    
+        $pos = ($registro * 4) + $index;
+        
+        $arrResultado[$registro + 1][$value] = preg_replace('/\s{2,}/', ' ', $nodes->item($pos)->nodeValue);
+    }
+}
+
+echo json_encode($arrResultado);
